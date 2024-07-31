4e72e6301f03cd9d4d89d7e8c260cebe57b73d68
Tausif <littletasteofheaven@protonmail.com>
2024-07-31 14:04:07 +0600

    Add fakerjs and update seed file

--- seeds/authors-books.js
+++ seeds/authors-books.js
@@ -1,20 +1,40 @@
+import { faker } from '@faker-js/faker';
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> } 
  */
-export const seed = async function(knex) {
-  await knex('books').del();
-  await knex('authors').del();
-  await knex('authors').insert([
-    {id: 1, name: 'Erich Maria Remark', bio: 'A war veteran', birthdate: '1896-05-25'},
-    {id: 2, name: 'Charles Dickens', bio: 'An English eriter', birthdate: '1893-12-04'},
-    {id: 3, name: 'Jules Varne', bio: 'A fiction novelist', birthdate: '1890-06-21'},
-  ]);
-  await knex('books').insert([
-    {id: 1, title: 'All Quiet On the Western Front', description: 'A book on WW1', published_date: '1945-07-22', author_id: 1},
-    {id: 2, title: 'Oliver Twist', description: 'Tale of an orphan boy', published_date: '1945-07-22', author_id: 2},
-    {id: 3, title: 'Around the World in 80 days', description: 'Fiction Novel', published_date: '1945-07-22', author_id: 3},
-    {id: 4, title: 'A journey to the center of the earth', description: 'Fiction Novel', published_date: '1945-07-22', author_id: 3},
-    {id: 5, title: 'David Copperfield', description: 'Tale of romance', published_date: '1945-07-22', author_id: 2},
-  ]);
-};
+export const seed = function(knex, Promise) {
+    return knex("authors").del()
+        .then(() => {
+            return knex("books").del();
+        })
+        .then(() => {
+            const authors = [];
+
+            for (let index = 0; index < 3; index++) {
+                authors.push({
+                    name: faker.lorem.sentence(),
+                    bio: faker.lorem.sentence(),
+                    birthdate: faker.date.recent()
+                });
+            }
+
+            return knex("authors").insert(authors);
+        })
+        .then(() => {
+            return knex('authors').pluck('id').then((postIds) => {
+                const books = [];
+
+                for (let index = 0; index < 10; index++) {
+                    books.push({
+                        title: faker.lorem.sentence(),
+                        description: faker.date.recent(),
+                        published_date: faker.date.recent(),
+                        author_id: faker.helpers.arrayElement(postIds)
+                    })
+                }
+
+                return knex("books").insert(books);
+            });                
+        })
+    };
